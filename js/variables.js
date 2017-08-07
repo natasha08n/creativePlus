ffae1dc2a424c17117411bbe453ba7037eb26c2f
Natalya <natashkachobot@mail.ru>
2017-08-07 11:08:32 +0300

    Add namespace to consts

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+//depends on function.js
+
+window.creativeConsts = window.creativeConsts || {
+    baseUrl: "http://localhost:3000/",
+
+    findPostId: +creativeFunctions.getParameterByName('postId'),
+    
+    //number of letters to preview All posts
+    allTextMaxSize: 400,
+    
+    //number of letters to preview Recent posts
+    recentTextMaxSize: 100,
+    
+    //default photo
+    photo: "https://res.cloudinary.com/creativeplus/image/upload/v1500449234/oek3puuce6nt0i3xsj2h.png"
+}
